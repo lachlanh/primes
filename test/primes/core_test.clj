158541b0059c1a8a2ed7e41484e568d18d407149
lachlanh <lhespe@opentable.com>
2018-03-17 21:16:30 -0700

    test first ten primes

--- test/primes/core_test.clj
+++ test/primes/core_test.clj
@@ -2,6 +2,8 @@
   (:require [clojure.test :refer :all]
             [primes.core :refer :all]))
 
+(def calculated-ten-primes (list 2 3 5 7 11 13 17 19 23 29))
+
 (deftest is-one-prime
   (testing "strict prime definition says 2 divisors, 1 is not a prime"
     (is (not (prime? 1)))))
@@ -9,3 +11,7 @@
 (deftest is-two-prime
   (testing "strict prime definition says 2 divisors"
     (is (prime? 2))))
+
+(deftest first-ten-primes
+  (testing "can we generate the first ten primes"
+    (is (= (ten-primes) calculated-ten-primes))))
